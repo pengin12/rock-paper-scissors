10c86967c41bd95762f3a0abc52cc07db531e3dc
pengin12 <137031551+pengin12@users.noreply.github.com>
2023-09-08 19:49:55 +0700

    Add getPlayerChoice function

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function getComputerChoice() {
   return Math.floor(Math.random() * 3);
 }
+
+function getPlayerChoice() {
+  let answer = prompt("Rock, Paper or Scissors", ).toLowerCase();
+  if (answer === "paper") {
+    return 0;
+  } else if (answer === "scissors") {
+    return 1;
+  } else if (answer === "rock") {
+    return 2;
+  } else {
+    return -1;
+  }
+}
