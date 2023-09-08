8f638aa3dd381223ac0a810781c63bd09e3ba957
pengin12 <137031551+pengin12@users.noreply.github.com>
2023-09-08 20:20:44 +0700

    Add game function and fix prompt null bug

--- script.js
+++ script.js
@@ -3,38 +3,78 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-  let answer = prompt("Rock, Paper or Scissors", ).toLowerCase();
-  if (answer === "paper") {
-    return 0;
-  } else if (answer === "scissors") {
-    return 1;
-  } else if (answer === "rock") {
-    return 2;
-  } else {
+  let answer = prompt("Rock, Paper or Scissors", )
+  if (answer === null) {
     return -1;
+  } else {
+    answer.toLowerCase();
+
+    if (answer === "paper") {
+      return 0;
+    } else if (answer === "scissors") {
+      return 1;
+    } else if (answer === "rock") {
+      return 2;
+    } else {
+      return -1;
+    }
   }
 }
 
 function playRound(playerSelection, computerSelection) {
   const playerWin = "Player win";
   const computerWin = "Computer win";
-
-  if (playerSelection === 0 && computerSelection === 2) {
-    console.log(playerWin);
-    return playerWin;
+  
+    if (playerSelection === -1) {
+      return "Game canceled";
+  } else if (playerSelection === 0 && computerSelection === 2) {
+      console.log(playerWin);
+      return playerWin;
   } else if (playerSelection === 2 && computerSelection === 0) {
-    console.log(computerWin);
-    return computerWin;
+      console.log(computerWin);
+      return computerWin;
   } else if (playerSelection > computerSelection) {
-    console.log(playerWin);
-    return playerWin;
+      console.log(playerWin);
+      return playerWin;
   } else if (computerSelection > playerSelection) {
-    console.log(computerWin);
-    return computerWin;
-  } else if (playerSelection === -1) {
-    console.log("Game canceled");
+      console.log(computerWin);
+      return computerWin;
   } else {
-    console.log("Tie");
-    return "Tie";
+      console.log("Tie");
+      return "Tie";
   }
 }
+
+function game() {
+  let playerScores = 0;
+  let computerScores = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+
+    const result = playRound(playerSelection, computerSelection);
+  
+    if (result === "Game canceled") {
+      console.log("Game canceled");
+      i = 5;
+    } else if (result === "Player win") {
+      ++playerScores;
+    } else if (result === "Computer win") {
+      ++computerScores;
+    }
+	}
+    
+  console.log(`Player ${playerScores} - ${computerScores} Computer`);
+  if (playerScores > computerScores) {
+    console.log("You win");
+  }
+  else if (computerScores > playerScores) {
+    console.log("Computer win");
+  }
+  else {
+    console.log("Tie");
+  }
+}
+
+game();
